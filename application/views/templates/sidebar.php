225369bb90454ce4b7ab2533b7836fbd3d604d42
AlifArnado <benden.alif@gmail.com>
2016-10-01 01:32:29 +0700

    sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,25 +1,31 @@
 <!-- Sidebar -->
 <div id="sidebar-wrapper" style="width: 300px;">
     <ul class="sidebar-nav">
-   <!--  <img src="<?php echo base_url("assets/images/logo.png"); ?>" style="margin-top: 20px;" class="img-responsive" alt="Image" > -->
+    <img src="<?php echo base_url("assets/images/logo.png"); ?>" style="margin-top: 20px; margin-left: 45px;" width="190" height="190" class="img-responsive" alt="Image" >
         <li class="sidebar-brand">
         </li>
 
-        <!-- <div class="sidebar-menu">
+        <div class="sidebar-menu">
            <img src="<?php echo base_url("assets/images/pita/P_1.png"); ?>" alt="" width="280" height="60">
            <div class="__menu">
-
+              <ul>
+                <li><a href="">Paket 1</a></li>
+                <li><a href="">Paket 2</a></li>
+                <li><a href="">Paket 3</a></li>
+                <li><a href="">Paket 4</a></li>
+              </ul>
            </div>
         </div>
 
         <div class="sidebar-menu">
            <img src="<?php echo base_url("assets/images/pita/P_2.png"); ?>" alt="" width="280" height="60">
            <div class="__menu">
+              <p class="text">www.bukmar.com adalah penjualan sembako online dengan harga yang lebih menarik, bonus yang banyak, hanya satu kali klik barang sampai di tempat anda!!</p>
 
            </div>
         </div>
 
-        <div class="sidebar-menu">
+        <!-- <div class="sidebar-menu">
            <img src="<?php echo base_url("assets/images/pita/P_3.png"); ?>" alt="" width="280" height="60">
            <div class="__menu">
 
